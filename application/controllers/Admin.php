a881f8068640e1c1a0375cf34b927d319818496e
Wilson Carpenter <wilsonkcarpenter@gmail.com>
2015-10-22 16:42:44 -0700

    Added validation to quote addition form

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -20,6 +20,14 @@ class Admin extends Application {
 	}
 
 	function present($quote) {
+		$message = '';
+		if (count($this->errors) > 0) 
+			foreach ($this->errors as $error)
+				$message .= $error . BR;
+
+		$this->data['message'] = $message;
+		
+
 		$this->data['fid'] = makeTextField('ID#', 'id', $quote->id, 
 			"Unique quote identifier, system-assigned", 10, 10,true);
 		$this->data['fwho'] = makeTextField('Author', 'who', $quote->who);
@@ -39,6 +47,17 @@ class Admin extends Application {
 		$record->who = $this->input->post('who');
 		$record->mug = $this->input->post('mug');
 		$record->what = $this->input->post('what');
+
+		if (empty($record->who)) 
+			$this->errors[] = 'You must specify an author';
+		if (strlen($record->what) < 20)
+			$this->errors[] = 'A quotation must be at least 20
+			 characters long.';
+
+		if (count($this->errors) > 0) {
+			$this->present($record);
+			return;
+		}
 
 		//save
 		if (empty($record->id))
