531e4cb7038a797192dd46b9933735eca17d7a3e
Wilson Carpenter <wilsonkcarpenter@gmail.com>
2015-10-22 16:04:17 -0700

    Added ability to add quotes. Validation is not completed yet.

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -20,13 +20,32 @@ class Admin extends Application {
 	}
 
 	function present($quote) {
-		$this->data['fid'] = makeTextField('ID#', 'id', $quote->id);
+		$this->data['fid'] = makeTextField('ID#', 'id', $quote->id, 
+			"Unique quote identifier, system-assigned", 10, 10,true);
 		$this->data['fwho'] = makeTextField('Author', 'who', $quote->who);
 		$this->data['fmug'] = makeTextField('Picture', 'mug', $quote->mug);
 		$this->data['fwhat'] = makeTextField('Quote', 'what', $quote->what);
+		$this->data['fsubmit'] = makeSubmitButton('Process Quote', 
+			"Click here to validate quote data", 'btn-success');
 		$this->data['pagebody'] = 'quote_edit';
 		$this->render();
+	}
 
+	function confirm() {
+		$record = $this->quotes->create();
 
+		//extract submitted fields
+		$record->id = $this->input->post('id');
+		$record->who = $this->input->post('who');
+		$record->mug = $this->input->post('mug');
+		$record->what = $this->input->post('what');
+
+		//save
+		if (empty($record->id))
+			$this->quotes->add($record);
+		else 
+			$this->quotes->update($record);
+
+		redirect('/admin');
 	}
 }
--- application/views/quote_edit.php
+++ application/views/quote_edit.php
@@ -1,8 +1,9 @@
 <div class="row">
-  <form action="/admin/confirm">
+  <form action="/admin/confirm" method="post">
     {fid}
     {fwho}
     {fmug}
     {fwhat}
+    {fsubmit}
   </form>
 </div>
